ac9785e66b662b4dd3f6a96d0a00fb5a97d8d574
Philipp Henkel <philipp.henkel@gmail.com>
2020-01-26 21:17:22 +0100

    Run podman rootless

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# MakerBit MicroPython Firmware for the BBC Micro:bit 
+# MakerBit MicroPython Firmware for the BBC Micro:bit
 
 
 ## How to build the firmware
 
 ```bash
-sudo podman build -t micropython-makerbit .
-sudo podman run --name makerbit micropython-makerbit
-sudo podman cp makerbit:/microbit-micropython/build/firmware.hex makerbit-python.hex
+podman build -t micropython-makerbit .
+podman run --name makerbit micropython-makerbit
+podman cp makerbit:/microbit-micropython/build/firmware.hex makerbit-python.hex
 ```
--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sudo podman build -t micropython-makerbit .
-sudo podman rm makerbit || true
-sudo podman run --name makerbit micropython-makerbit
-sudo mkdir build
-sudo podman cp makerbit:/microbit-micropython/build/firmware.hex build/makerbit-python.hex
-sudo podman cp makerbit:/tmp/usedspace.txt build/makerbit-python-usedspace.txt
+podman build -t micropython-makerbit .
+podman rm makerbit || true
+podman run --name makerbit micropython-makerbit
+mkdir build || true
+podman cp makerbit:/microbit-micropython/build/firmware.hex build/makerbit-python.hex
+podman cp makerbit:/tmp/usedspace.txt build/makerbit-python-usedspace.txt
+echo "Successfully completed."
